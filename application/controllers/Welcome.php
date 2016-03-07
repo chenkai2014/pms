cad573f1a6cfc3629e6f045b94f1293466bc1635
DaviderAc <137618239@qq.com>
2016-03-07 22:22:00 +0800

    增加了用户登出功能

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -23,6 +23,23 @@ class Welcome extends CI_Controller {
 		$this->load->view('welcome_message');
 	}
 
+	//用户注册
+    public function register()
+	{
+
+	}
+
+	//用户登出
+	public function logout()
+	{
+       unset($_SESSION['member_id']);
+	   unset($_SESSION['member_name']);
+	   unset($_SESSION['is_login']);
+
+	   header('Location:http://localhost/index.php/Welcome/index');
+	}
+
+	//用户登录
 	public function login()
 	{
         $username=$_POST['username'];
@@ -35,8 +52,10 @@ class Welcome extends CI_Controller {
 
 		if(count($data['member_info'])==1)
 		{
-			//将用户ID放入Session
+			//将用户信息放入Session
+			$_SESSION['is_login']=1;
 			$_SESSION['member_id']=$data['member_info'][0]['member_id'];
+			$_SESSION['member_name']=$data['member_info'][0]['name'];
 
 			$this->load->view('templates/header',$data);
 			$this->load->view('member_home',$data);
